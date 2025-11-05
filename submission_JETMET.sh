d7a6883009ac53bc00db66534866a53dc2c7d758
Mitanshu Thakore <mitanshu.thakore@cern.ch>
2025-11-05 13:42:50 -0600

    Updated

--- submission_JETMET.sh
+++ submission_JETMET.sh
@@ -1,47 +1,47 @@
-### C
+## C
 
 
-# python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET0/Run2024C-MINIv6NANOv15-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
+python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET0/Run2024C-MINIv6NANOv15-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET1/Run2024C-MINIv6NANOv15-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
-### D
+## D
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET0/Run2024D-MINIv6NANOv15-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET1/Run2024D-MINIv6NANOv15-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
-### E
+## E
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET0/Run2024E-MINIv6NANOv15-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET1/Run2024E-MINIv6NANOv15-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
-### F
+## F
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET0/Run2024F-MINIv6NANOv15-v2/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET1/Run2024F-MINIv6NANOv15-v2/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
-### G
+## G
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET0/Run2024G-MINIv6NANOv15-v2/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET1/Run2024G-MINIv6NANOv15-v2/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
-### H
+## H
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET0/Run2024H-MINIv6NANOv15-v2/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET1/Run2024H-MINIv6NANOv15-v2/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
-### I
+## I
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET0/Run2024I-MINIv6NANOv15-v2/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET0/Run2024I-MINIv6NANOv15_v2-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
-python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET1/Run2024I-MINIv6NANOv15-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
+python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET1/Run2024I-MINIv6NANOv15-v1/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_MET120_NoiseFilters_Skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
 python3 skimNtuples.py --skimFileConfiguration JETMET.json --datasetKey /JetMET1/Run2024I-MINIv6NANOv15_v2-v2/NANOAOD --skimCutConfiguration 2024_looseGeneralSkimCuttingConfiguration.json --skimBranchCancelations Null_Cancelation.json --destination /hdfs/store/user/mithakor/2024_skimmed --prepareCondorSubmission --submitDirPath /nfs_scratch/mithakor/condor-jobs
 
